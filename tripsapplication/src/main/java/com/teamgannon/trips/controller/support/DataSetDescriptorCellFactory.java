12d15415d88ba3a7a536d13511c9df2d0085c802
larrymitchell <larry.mitchell@rocketbnk.com>
2020-08-19 22:51:46 -0400

    release v0.5 added a lot of changes to work with preferences technical debt

---
+++ tripsapplication/src/main/java/com/teamgannon/trips/controller/support/DataSetDescriptorCellFactory.java
@@ -0,0 +1,23 @@
+package com.teamgannon.trips.controller.support;
+
+import com.teamgannon.trips.jpa.model.DataSetDescriptor;
+import com.teamgannon.trips.search.StellarDataUpdater;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+
+public class DataSetDescriptorCellFactory implements Callback<ListView<DataSetDescriptor>, ListCell<DataSetDescriptor>> {
+
+    private StellarDataUpdater updater;
+
+    public DataSetDescriptorCellFactory(StellarDataUpdater updater){
+        this.updater = updater;
+    }
+
+    @Override
+    public ListCell<DataSetDescriptor> call(ListView<DataSetDescriptor> dataSetDescriptorListView) {
+        return new DataSetDescriptorCell(updater);
+    }
+
+}
