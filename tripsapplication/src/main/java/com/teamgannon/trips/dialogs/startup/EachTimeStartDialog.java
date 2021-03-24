94c964892c722741706a0ff3510b68cc0d2f7525
Larry Mitchell <larry.mitchell@rocketbnk.com>
2021-03-23 21:43:19 -0400

    v0.6.8 changes
    polities

--- tripsapplication/src/main/java/com/teamgannon/trips/dialogs/startup/EachTimeStartDialog.java
+++ tripsapplication/src/main/java/com/teamgannon/trips/dialogs/startup/EachTimeStartDialog.java
@@ -18,7 +18,7 @@ public class EachTimeStartDialog extends Dialog<Boolean> {
         Font font = Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 15);
 
         VBox vBox = new VBox();
-        Label title = new Label("Terran Interstellar Plotting System");
+        Label title = new Label("Terran Republic Interstellar Plotting System");
         title.setFont(font);
         vBox.getChildren().add(title);
         vBox.getChildren().add(new Separator());
